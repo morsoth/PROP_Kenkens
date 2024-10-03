573a01c2cf8c38abc5acd00e84a2beccff5eb11f
morsoth <94766863+morsoth@users.noreply.github.com>
2024-10-03 10:10:19 +0200

    Tercera Entrega

---
+++ Tercera Entrega/DOCS/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"edu.upc.prop.clusterxx"},{"l":"edu.upc.prop.clusterxx.domain"},{"l":"edu.upc.prop.clusterxx.domain.operations"},{"l":"edu.upc.prop.clusterxx.exceptions"},{"l":"edu.upc.prop.clusterxx.persistence"},{"l":"edu.upc.prop.clusterxx.presentation"}]
